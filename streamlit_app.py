82e4725263980c62c5ff6569679f2a5c7169258e
AayushiSF <109787258+AayushiSF@users.noreply.github.com>
2022-07-22 14:12:00 +0530

    Update streamlit_app.py
    Added Body

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
+streamlit.head('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kate, Spinach & Rocket Smoothie')
+streamlit.text('Hard—boiled Egg')
+
