325efc018d81d7347fc323dfe8a40eeb0554e3ab
AayushiSF <109787258+AayushiSF@users.noreply.github.com>
2022-07-22 14:14:31 +0530

    Update streamlit_app.py
    Adding Emoticon

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kate, Spinach & Rocket Smoothie')
-streamlit.text('Hard—boiled Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kate, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞Hard—boiled Egg')
 
