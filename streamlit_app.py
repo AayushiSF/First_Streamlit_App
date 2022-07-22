aaaedb1e3106b8bd6912560ce60f6bbe57a29ec8
AayushiSF <109787258+AayushiSF@users.noreply.github.com>
2022-07-22 14:12:51 +0530

    Update streamlit_app.py
    Correction in Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
-streamlit.head('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kate, Spinach & Rocket Smoothie')
 streamlit.text('Hard—boiled Egg')
