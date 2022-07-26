e3b6a1642a1d781721832aa756f4668e61be598e
AayushiSF <109787258+AayushiSF@users.noreply.github.com>
2022-07-26 17:57:23 +0530

    Update streamlit_app.py
    Correction in Schema and role

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * FROM fruit_load_list")
+my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("Fruit Load List Contains:")
 streamlit.text(my_data_row)
