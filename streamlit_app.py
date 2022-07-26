bf1f2f66f46424970665ed95ad84cb2552817c09
AayushiSF <109787258+AayushiSF@users.noreply.github.com>
2022-07-26 18:12:41 +0530

    Update streamlit_app.py
    adding values to SF table via Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,5 +59,8 @@ streamlit.dataframe(my_data_rows)
 
 #ALlow the end user to add the fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('from streamlit')")
+
+
