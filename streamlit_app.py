0863866b91e975e308cacee50d83aadd4a9310f6
AayushiSF <109787258+AayushiSF@users.noreply.github.com>
2022-07-27 12:37:03 +0530

    Update streamlit_app.py
    # Dont run anything past here while we troubleshoot
    streamlit.stop()

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,9 +55,11 @@ try:
 except URLError as e:
     streamlit.error()
     
- streamlit.stop()   
+ 
 
-
+# Dont run anything past here while we troubleshoot
+streamlit.stop()   
+# import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
