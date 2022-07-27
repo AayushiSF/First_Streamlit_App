b7b3601c97e5d73b992ead46cdf66de6c3082206
AayushiSF <109787258+AayushiSF@users.noreply.github.com>
2022-07-27 12:33:15 +0530

    Update streamlit_app.py
    Correcting typo mistake "if not fruit choice:"

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,11 +41,15 @@ streamlit.header("Fruityvice Fruit Advice!")
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit choice:
+  if not fruit_choice:
       streamlit.error("Please select a fruit to get the information")  
   else:
+      #streamlit.write('The user entered ', fruit_choice)
+      # import requests
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # Normalizing the JSON Version of API Response
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # Presenting the Normalized output as Table
       streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
